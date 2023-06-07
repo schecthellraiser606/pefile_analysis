536ae1ac250014100bd577786016e4ab8c71541b
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-07 18:21:06 +0900

    sectionget

--- pe_section_display.py
+++ pe_section_display.py
@@ -5,8 +5,9 @@ malw_file = sys.argv[1]
 pe = pefile.PE(malw_file)
 
 for section in pe.sections:
-  print(" %s %s %s %s" % (section.Name,
+  print(" %s %s %s %s %s" % (section.Name,
                           hex(section.VirtualAddress),
                           section.Misc_VirtualSize,
-                          section.SizeOfRawData))
+                          section.SizeOfRawData,
+                          section.get_entropy()))
 print("\n")
